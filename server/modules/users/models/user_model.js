c65c908d9b1e9c22515de540f01fa2a2b3baf586
Sukum <kungo2009@hotmail.com>
2018-05-17 16:45:52 +0700

    add owasp test password.

--- server/modules/users/models/user_model.js
+++ server/modules/users/models/user_model.js
@@ -3,6 +3,14 @@
 import mongoose from 'mongoose'
 import crypto from 'crypto';
 const Schema = mongoose.Schema
+const owasp = require('owasp-password-strength-test')
+owasp.config({
+    allowPassphrases: true,
+    maxLength: 128,
+    minLength: 10,
+    minPhraseLength: 20,
+    minOptionalTestsToPass: 4,
+});
 
 const userSchema = new Schema({
     firstname: {
@@ -39,22 +47,38 @@ const userSchema = new Schema({
 
 })
 
-userSchema.pre('validate', function(next) {
-    if (this.password && this.salt) {
-        this.salt = crypto.randomBytes(256).toString('base64');
-        this.password = this.hashPassword(this.password)
-    }
-    next();
-})
+userSchema.pre('save', function (next) {
+    if (this.password) {
+        let owasp_test = this.owaspFunction(this.password)
+        if (owasp_test.status) {
+            this.salt = crypto.randomBytes(256).toString('base64');
+            this.password = this.hashPassword(this.password)
+            next();
 
-userSchema.methods.hashPassword = function(password) {
+        } else {
+            next(owasp_test.err.errors[0])
+        }
+    }
+})
+userSchema.methods.owaspFunction = function (password) {
+    let result = owasp.test(password)
+    if (result.strong) {
+        return { status: true }
+    }
+    else {
+        return { err: result, status: false };
+    }
+
+}
+
+userSchema.methods.hashPassword = function (password) {
 
     if (password && this.salt) {
         return crypto.pbkdf2Sync(password, this.salt, 10000, 64, 'sha512').toString('base64');
     }
 
 }
-userSchema.methods.authenticate = function (password, userpassword)  {
+userSchema.methods.authenticate = function (password, userpassword) {
 
     return this.password === this.hashPassword(password);
 
