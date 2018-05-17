1cdb9d66ca17eef0b1ec7cd2de4363dbaf26b87e
Sukum <kungo2009@hotmail.com>
2018-05-17 17:37:03 +0700

    add img for home page and add msg err signup

--- client/src/app/components/auth/sign-up/sign-up.component.ts
+++ client/src/app/components/auth/sign-up/sign-up.component.ts
@@ -10,7 +10,11 @@ import { Router } from '@angular/router';
 })
 export class SignUpComponent implements OnInit {
   auth = {};
-  constructor(private user: UserSeriveService, private router : Router) { }
+  firstname: String = '';
+  lastname: String = '';
+  getError: String;
+  errSHow: boolean = false;
+  constructor(private user: UserSeriveService, private router: Router) { }
 
   ngOnInit() {
     if (this.user.auth()) {
@@ -20,9 +24,16 @@ export class SignUpComponent implements OnInit {
     }
   }
   onSignUp() {
-    this.user.serviceSignUp(this.auth).subscribe(data => console.log(data))
+
+    /*this.user.serviceSignUp(this.auth).subscribe(data => localStorage.setItem("id_token", data), err => {
+      this.errSHow = true;
+      this.getError = err.error.message;
+    })*/
+    console.log()
 
 
   }
-
+  onCloseError() {
+    this.errSHow = false
+  }
 }
